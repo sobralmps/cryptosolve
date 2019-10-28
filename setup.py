d8d8b32dda2ab0f4a548961c1c9ef4b7e60865bf
Marcio <sobralmps@gmail.com>
2019-10-28 10:56:40 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='djangoCryptoSolve',
+    version='',
+    packages=['django'],
+    url='',
+    license='',
+    author='marcio.sobral',
+    author_email='',
+    description=''
+)
