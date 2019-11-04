6156f5f87399b377af1d02e4f4c8f8fb76568dc0
sobralmps <sobralmps@gmail.com>
2019-11-04 06:44:43 -0300

    Setup.py edit

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='djangoCryptoSolve',
     version='',
-    packages=['django'],
+    install_requires=['django'],
+    packages=find_packages(),
     url='',
     license='',
     author='marcio.sobral',
