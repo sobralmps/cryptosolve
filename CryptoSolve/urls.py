2599c678204e1382c8f6c7d93cf6bb536174cd02
murillenda <giikthelol@gmail.com>
2019-11-05 11:23:38 -0300

    Atualizando as pages

--- CryptoSolve/urls.py
+++ CryptoSolve/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from crypto.views import test2, test3, test1
+from crypto.views import test1
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',test1,name='url_test1'),
-    path('test2/',test2,name='url_test2'),
-    path('test3/', test3, name='url_test3'),
 
 ]
--- crypto/views.py
+++ crypto/views.py
@@ -2,9 +2,3 @@ from django.shortcuts import render, redirect
 
 def test1(request):
     return render(request, 'crypto/test1.html')
-
-def test2(request):
-    return render(request, 'crypto/test2.html')
-
-def test3(request):
-    return render(request, 'crypto/test3.html')
