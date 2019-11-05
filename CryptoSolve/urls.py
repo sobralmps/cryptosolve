125221ebf994e809317c783c5d59a8ff6010bbd1
murillenda <giikthelol@gmail.com>
2019-11-05 11:43:52 -0300

    Criação de nova pagina

--- CryptoSolve/urls.py
+++ CryptoSolve/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from crypto.views import test1
+from crypto.views import test1, test2
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',test1,name='url_test1'),
+    path('menu', test2, name='url_test2'),
 
 ]
--- crypto/views.py
+++ crypto/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def test1(request):
     return render(request, 'crypto/test1.html')
+
+def test2(request):
+    return render(request, 'crypto/test2.html')
