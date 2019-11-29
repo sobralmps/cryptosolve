e1b85da86b5fb4ecaaf09ddce1b1db7e4e1351e2
sobralmps <sobralmps@gmail.com>
2019-11-29 08:26:32 -0300

    --só terminando de organizar--

--- crypto/static/crypto/js/game.js
+++ crypto/static/crypto/js/game.js
@@ -2,7 +2,7 @@ function checar(form){
     cryptotext = form.cryptotext.value;
     if (cryptotext == "hello world"){
         alert("Indo para a fase 02")
-        window.location.href="../endgame";
+        window.location.href="../embreve";
         }
     else {
         alert("Incorreto.")
