2c7818d8720653cd52d85e003f708c192547b0af
sobralmps <sobralmps@gmail.com>
2019-11-30 11:45:59 -0300

    Att página conquistas

--- crypto/static/crypto/js/conquistas.js
+++ crypto/static/crypto/js/conquistas.js
@@ -1,8 +1,8 @@
 function conquistas(){
 
-    var x = 0;
-    var y = 0;
-    var z = 0;
+    var conq1 = 1;
+    var conq2 = 1;
+    var conq3 = 0;
 
     var conquistas = new Array();
 
@@ -10,13 +10,13 @@ function conquistas(){
     conquistas[1] = "Completou as fases com cifra de Cesar";
     conquistas[2] = "Completou a ultima fase";
 
-    if (x == 0){
+    if (conq1 == 1){
         document.getElementById("Conquista1").innerHTML = conquistas[0];
     }
-    if (y == 0){
+    if (conq2 == 1){
         document.getElementById("Conquista2").innerHTML = conquistas[1];
     }
-    if (z == 0){
+    if (conq3 == 1){
         document.getElementById("Conquista3").innerHTML = conquistas[2];
     }
 
